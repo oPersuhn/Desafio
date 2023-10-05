cc7ad4f5f1e4fd17389459c2d3237bf2737bdd0b
oPersuhn <augusto.persuhn1@gmail.com>
2023-10-05 17:05:51 -0300

    Alterações em andamento

--- script.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-document.write("hello world");
---
+++ script.ts
@@ -0,0 +1,18 @@
+
+function Cadastrar(){
+    let nome = document.getElementById(
+        'nome',
+      ) as HTMLInputElement;
+      const cpf = document.getElementById(
+        'cpf',
+      ) as HTMLInputElement;
+      const nascimento = document.getElementById(
+        'nascimento',
+      ) as HTMLInputElement;
+      const nomeVacina = document.getElementById(
+        'nomeVacina',
+      ) as HTMLInputElement;
+      const dataVacina = document.getElementById(
+        'dataVacina',
+      ) as HTMLInputElement;
+}
